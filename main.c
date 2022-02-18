9a90e6c532a0d6c63a8803584e0b279188e4627b
lauk20 <klau20@stuy.edu>
2022-02-17 21:54:19 -0500

    Added tester code

--- main.c
+++ main.c
@@ -7,6 +7,41 @@
 #include "draw.h"
 #include "matrix.h"
 
+int main(){
+  printf("\nTesting add_edge. Adding (1,2,3), (4,5,6) m2 =\n");
+
+  struct matrix * test;
+  test = new_matrix(4,10);
+  add_edge(test, 1, 2, 3, 4, 5, 6);
+  print_matrix(test);
+
+  printf("\n");
+
+  printf("Testing ident. m1 =\n");
+  struct matrix * identm;
+  identm = new_matrix(4,4);
+  ident(identm);
+  print_matrix(identm);
+
+  printf("\n");
+
+  printf("Testing Matrix mult. m1 * m2 =\n");
+  matrix_mult(identm, test);
+  print_matrix(test);
+
+  struct matrix * m1;
+
+  m1 = new_matrix(4, 10);
+  add_edge(m1, 1, 2, 3, 4, 5, 6);
+  add_edge(m1, 7, 8, 9, 10, 11, 12);
+  printf("\nTesting Matrix mult. m1 =\n");
+  print_matrix(m1);
+  printf("\nTesting Matrix mult. m1 * m2 =\n");
+  matrix_mult(m1, test);
+  print_matrix(test);
+}
+
+/*
 int main() {
 
   screen s;
@@ -15,4 +50,5 @@ int main() {
   edges = new_matrix(4, 4);
 
   free_matrix( edges );
-}  
+}
+*/
